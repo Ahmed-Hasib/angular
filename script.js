756fc62a181b26ccae046468b53f62546cbec841
hasibahmed171 <hasibahmed999@gmail.com>
2018-01-18 13:55:59 +0600

    angularJS try

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var myapp=angular.module("mymodule",[]);
 myapp.controller("mycontroller",function($scope){
     $scope.msg="AngularJS is Working ";
     var employee=[
-        {name:"Hasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.223 },
-        {name:"Hasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.254 },
-        {name:"Hasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.223 },
-        {name:"Hasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.254 },
-        {name:"Hasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.223 },
+        {name:"Aasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.223 },
+        {name:"Basib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.254 },
+        {name:"Casib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.223 },
+        {name:"Dasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.254 },
+        {name:"Easib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.223 },
         {name:"Hasib Ahmed",dob:new Date("November 28,1990"),gender:"male",salary:50000.254 }
     ];
     $scope.employees=employee;
